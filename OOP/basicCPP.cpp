e3d5e399acdc0cc2cf761a9aa86bacfaa61a37a2
pratik kagale <pratikkagale3333@gmail.com>
2023-08-31 23:51:48 +0530

    Cpp program

---
+++ OOP/basicCPP.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int no;
+    cout<<"\nEnter Number";
+    cin>>no;
+    if (no%2==0)
+    {
+        cout<<"Number is Even Number";
+    }
+    else
+    {
+        cout<<"Number is Odd";
+    }
+    return 0;
+}
