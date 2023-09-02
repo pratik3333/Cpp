fe754aabf5b27f2b36dea1e2fb61272296dd2bd0
pratik kagale <pratikkagale3333@gmail.com>
2023-09-02 18:46:17 +0530

    cpp programs

--- Function/square.cpp
+++ Function/square.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 int main()
 {
-    
     int no;
     int square(int);
     cout<<"\nEnter number\n";
     cin>>no;
     int result=square(no);
     cout<<"\nSquare is "<<result;
-    
     return 0;
 }
 
